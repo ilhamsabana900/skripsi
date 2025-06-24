9655b4ca13340e2537463409d1ddfee5475eab23
ilhaaams- <ilhamsabana900@gmail.com>
2025-06-24 14:21:46 +0700

    akumulasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,9 +40,9 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2  bg-white my-2" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand px-4 py-3 m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
-        <img src="#" class="navbar-brand-img" width="26" height="26" >
-        <span class="ms-1 text-sm text-dark">PRODISTIK MAN 1 PASURUAN</span>
+      <a class="navbar-brand px-4 py-3 m-0"  target="_blank">
+        <img src="{{ asset('img/prodistik.jpeg') }}" class="navbar-brand-img" width="26" height="26" >
+        <span class="ms-1 text-sm text-dark">PRODISTIK MANSAPAS</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0 mb-2">
